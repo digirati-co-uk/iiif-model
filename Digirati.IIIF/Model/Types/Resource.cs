d66432e1f84cf2df44366ee408af7e0f9f60c157
Tom Crane <tom@nanoplanet.com>
2015-09-19 15:42:07 +0100

    Reinstate JSONProperty "format" for resource - why did this go?

--- Digirati.IIIF/Model/Types/Resource.cs
+++ Digirati.IIIF/Model/Types/Resource.cs
@@ -7,10 +7,14 @@ namespace Digirati.IIIF.Model.Types
     [JsonConverter(typeof(ResourceSerialiser))]
     public class Resource : JSONLDBase
     {
-        [JsonProperty(Order = 11, PropertyName = "label")]
+        [JsonProperty(Order = 10, PropertyName = "format")]
+        public string Format { get; set; }
+
+        [JsonProperty(Order = 11, PropertyName = "profile")]
+        public virtual dynamic Profile { get; set; }
+
+        [JsonProperty(Order = 20, PropertyName = "label")]
         public MetaDataValue Label { get; set; }
 
-        //[JsonProperty(Order = 10, PropertyName = "format")]
-        public string Format { get; set; }
     }
 }
