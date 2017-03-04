669304fd68ef1a331ba29d5930da025835b830cb
tomcrane <tom@nanoplanet.com>
2017-03-04 11:24:15 +0000

    Support for Illustration annotations

---
+++ Digirati.IIIF/Model/Extension/IllustrationAnnotationResource.cs
@@ -0,0 +1,11 @@
+﻿using Digirati.IIIF.Model.JsonLD;
+using Newtonsoft.Json;
+
+namespace Digirati.IIIF.Model.Extension
+{
+    public class IllustrationAnnotationResource : JSONLDBase
+    {
+        [JsonProperty(Order = 10, PropertyName = "label")]
+        public string Label { get; set; }
+    }
+}
